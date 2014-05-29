f66d4ba78fa13dbf3c8e279f51f41b3296e27ec3
Lachlan Donald <lachlan@ljd.cc>
2014-05-29 21:55:36 +1000

    Store only sublime settings

--- sublime/install.sh
+++ sublime/install.sh
@@ -2,12 +2,11 @@
 
 cd "$(dirname $0)"
 
-sbsettings="$HOME/Library/Application Support/Sublime Text 3"
+settings_dir="$HOME/Library/Application Support/Sublime Text 3/Packages/User"
 
-if [ ! -L "$sbsettings/Packages/User" ]; then
-  ln -s ./User "$sbsettings/Packages/User"
-fi
-
-if [ ! -f "$sbsettings/Installed Packages/Package Control.sublime-package" ]; then
-  wget https://sublime.wbond.net/Package%20Control.sublime-package
-fi
+# link Sublime Text settings
+find `pwd` -iname "*.sublime-settings" | while read f
+do
+	ln -sf "$f" "$settings_dir/$(basename $f)"
+  echo linked $settings_dir/$(basename $f)
+done
