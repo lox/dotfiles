df9cc57f663bd82f76e11d6925fe5e3840304eaa
Lachlan Donald <lachlan@ljd.cc>
2015-04-28 12:34:22 +1000

    Removed Chrome

--- osx/install.sh
+++ osx/install.sh
@@ -4,14 +4,8 @@ export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
 # desktop apps
 brew cask install alfred
-brew cask install google-chrome
 brew cask install iterm2
 brew cask install rdio
 brew cask install sequel-pro
 brew cask install skype
 brew cask install transmission
-
-# terminal apps
-brew install pv
-brew install mysql
-brew install jq
