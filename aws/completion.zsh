ea56d039ef4c081c6a8ed1552a9e49806488a06d
Lachlan Donald <lachlan@ljd.cc>
2015-04-28 12:06:16 +1000

    Updated stuff

--- aws/completion.zsh
+++ aws/completion.zsh
@@ -4,8 +4,8 @@ function _aws_profiles {
   reply=( default $(grep profile $AWS_HOME/config | sed -e 's/.*profile \([a-zA-Z0-9_-]*\).*/\1/') )
 }
 
-if [ -f /usr/local/bin/aws_zsh_completer.sh ] ; then
-  source /usr/local/bin/aws_zsh_completer.sh
+if [ -f "$(brew --prefix)/opt/awscli/libexec/bin/aws_zsh_completer.sh" ] ; then
+  source $(brew --prefix)/opt/awscli/libexec/bin/aws_zsh_completer.sh
 fi
 
 compctl -K _aws_profiles awp
