21066094c6d3f42f7c4c3b15c36708b933ee0fa9
Lachlan Donald <lachlan@ljd.cc>
2016-02-01 10:05:36 +1100

    Add dm_env helper for docker-machine

---
+++ docker/env.zsh
@@ -0,0 +1,18 @@
+
+dm_env() {
+  local machine=${1:-99dev}
+
+  eval $(docker-machine inspect ${machine} --format \
+  "export DOCKER_HOST=tcp://{{ .Driver.IPAddress }}:2376
+  export DOCKER_TLS_VERIFY=1
+  export DOCKER_CERT_PATH={{ .HostOptions.AuthOptions.StorePath }}
+  export DOCKER_MACHINE_NAME={{ .Name }}
+  ")
+
+  export RPROMPT="<docker:${machine}>"
+}
+
+dm_env_unset() {
+  eval $(docker-machine env --unset)
+  unset RPROMPT
+}
