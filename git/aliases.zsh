1c047770500ab93b52e5c0e7ffe1aae6ff6f3531
Lachlan Donald <lachlan@ljd.cc>
2016-11-19 12:35:30 +1100

    Add git_assume and identities

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,12 +1,4 @@
 
-# Use `hub` as our git wrapper:
-#   http://defunkt.github.com/hub/
-hub_path=$(which hub)
-if (( $+commands[hub] ))
-then
-  alias git=$hub_path
-fi
-
 alias gl='git pull --rebase'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push'
---
+++ git/identity.zsh
@@ -0,0 +1,37 @@
+
+git_identities() {
+  git config --get-regexp '^identity\.' | cut -d"." -f2 | sort -u
+}
+
+git_assume() {
+  local identity="$1"
+  local name
+  local email
+
+  if ! name=$(git config "identity.$identity.name") ; then
+    echo "Missing name for $identity"
+    return 1
+  fi
+
+  if ! email=$(git config "identity.$identity.email") ; then
+    echo "Missing email for $identity"
+    return 1
+  fi
+
+  git config user.identity "$identity"
+  git config user.name "$name"
+  git config user.email "$email"
+}
+
+hub_path=$(which hub)
+if (( $+commands[hub] )) ; then
+  hub() {
+    local identity
+    local token
+
+    identity=$(git config "user.identity")
+    token=$(git config "identity.${identity}.githubtoken")
+
+    GITHUB_TOKEN=$token command hub "$@"
+  }
+fi
