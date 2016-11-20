c2c285968faf0ce28622169b6a5ca730a157289c
Lachlan Donald <lachlan@ljd.cc>
2016-11-21 09:17:23 +1100

    Remove old aws helpers, add aws-vault completion

--- aws/completion.zsh
+++ aws/completion.zsh
@@ -1,12 +1,5 @@
 
-# auto-complete for awp
-function _aws_profiles {
-  reply=( $(awk '/^\[/ {print $(NF)}' ~/.aws/config | tr -d '[]') )
-}
-
 # auto-complete for aws cli
 if [ -f "/usr/local/opt/awscli/libexec/bin/aws_zsh_completer.sh" ] ; then
   source /usr/local/opt/awscli/libexec/bin/aws_zsh_completer.sh
 fi
-
-compctl -K _aws_profiles awp
