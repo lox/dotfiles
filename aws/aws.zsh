6f1f91c496710d849382d596c3e76f97be935a57
Lachlan Donald <lachlan@ljd.cc>
2017-05-19 11:24:52 +1000

    Add an ecr-login function

--- aws/aws.zsh
+++ aws/aws.zsh
@@ -4,3 +4,15 @@ if [[ -n "$AWS_VAULT" ]] ; then
 fi
 
 export AWS_VAULT_PROMPT=osascript
+
+ecr-login() {
+  local profile="${1:-}"
+  if [[ -n "$profile" ]] ; then
+    eval "$(aws-vault exec "$profile" -- aws ecr get-login)"
+  elif [[ -n "$AWS_VAULT" ]] ; then
+    eval "$(aws ecr get-login)"
+  else
+    echo "No aws credentials or profile provided"
+    return 1
+  fi
+}
