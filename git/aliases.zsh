cc0f7fc9c569943e380240ff75fc7f149efca3f7
Lachlan Donald <lachlan@ljd.cc>
2017-05-19 11:26:12 +1000

    Updated git settings and added git-clean-all

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -9,3 +9,11 @@ alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb'
 alias gfff='git fetch && git ff'
+
+git-clean-all() {
+  git clean -xfd
+  git submodule foreach --recursive git clean -xfd
+  git reset --hard
+  git submodule foreach --recursive git reset --hard
+  git submodule update --init --recursive
+}
