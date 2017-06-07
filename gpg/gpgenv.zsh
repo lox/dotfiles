7b0bba8fb7f2da5905364f081412ed90634cd3dd
Lachlan Donald <lachlan@ljd.cc>
2017-06-07 18:26:43 +1000

    Added gpgenv functions

--- gpg/gpgenv.zsh
+++ gpg/gpgenv.zsh
@@ -6,11 +6,33 @@ gpgenv() {
 
   if [[ ! -f $f ]] ; then
     echo "Can't find $f"
-    exit 1
+    return 1
   fi
 
   (
   eval "$(gpg --decrypt $f)"
   eval "$@"
   )
+}
+
+gpgenc() {
+  gpg -ac "$@"
+}
+
+gpgcat() {
+  gpg --decrypt "$@"
+}
+
+gpgedit() {
+  set -x
+  local f="$1"
+  local tmpfile=".envtmp$$"
+
+  if [[ -f "$f" ]] ; then
+    gpg --decrypt "$f" > "$tmpfile"
+  fi
+
+  vim "$tmpfile"
+  gpg -ac -o "$f" "$tmpfile"
+  rm $tmpfile
 }
