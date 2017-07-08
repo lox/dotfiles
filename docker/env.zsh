5e6999a049d2c685025a31c1ef44bfff0bf10f01
Lachlan Donald <lachlan@ljd.cc>
2017-07-08 22:38:18 +1000

    Remove docker machine stuff

--- docker/env.zsh
+++ docker/env.zsh
@@ -1,22 +1,5 @@
 
-dm_env() {
-  local machine=${1:-99dev}
-
-  eval $(docker-machine inspect ${machine} --format \
-  "export DOCKER_HOST=tcp://{{ .Driver.IPAddress }}:2376
-  export DOCKER_TLS_VERIFY=1
-  export DOCKER_CERT_PATH={{ .HostOptions.AuthOptions.StorePath }}
-  export DOCKER_MACHINE_NAME={{ .Name }}
-  ")
-
-  export RPROMPT="<docker:${machine}>"
-}
-
-dm_env_unset() {
-  eval $(docker-machine env --unset)
-  unset RPROMPT
-}
-
+alias d=docker
 alias dc=docker-compose
 alias dce=docker-compose exec
 alias dcr=docker-compose run --rm
