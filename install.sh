5549584706c5c7e8220e4a30d6d09d8db800ad35
Lachlan Donald <lachlan@ljd.cc>
2017-07-19 20:25:03 +1000

    Always fail if install script fails

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ brew bundle install
 
 find "$DOTFILES" -name 'install.sh' -mindepth 2 -print0 | while read -d $'\0' file; do
   echo -e "\\n> Running installer $file"
-  bash $file
+  bash -eu -o pipefail $file
 done
 
 echo "Done."
