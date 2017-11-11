b5b50da9a1b8ce7cdaa0829fcae219abd05b1a74
Lachlan Donald <lachlan@ljd.cc>
2017-11-12 09:51:44 +1100

    Get commit signing working again

--- gpg/install.sh
+++ gpg/install.sh
@@ -3,3 +3,7 @@
 test -L ~/.gnupg/gpg-agent.conf || {
   ln -sfv "$DOTFILES"/gpg/gpg-agent.conf ~/.gnupg/gpg-agent.conf
 }
+
+test -L ~/.gnupg/gpg.conf || {
+  ln -sfv "$DOTFILES"/gpg/gpg.conf ~/.gnupg/gpg.conf
+}
