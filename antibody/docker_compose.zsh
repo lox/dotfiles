26a9674888d1c9ae76deaef2492d73c5bf315d7b
Lachlan Donald <lachlan@ljd.cc>
2018-06-11 13:08:27 +0200

    Remove emoji from antibody plugins

--- antibody/docker_compose.zsh
+++ antibody/docker_compose.zsh
@@ -5,7 +5,7 @@
 GEOMETRY_DOCKER_MACHINE_FAILED_COLOR=${GEOMETRY_DOCKER_MACHINE_FAILED_COLOR:-red}
 
 # Symbol definitions
-GEOMETRY_DOCKER_COMPOSE_AVAILABLE_SYMBOL=${GEOMETRY_DOCKER_COMPOSE_AVAILABLE_SYMBOL:-"🐳"}
+GEOMETRY_DOCKER_COMPOSE_AVAILABLE_SYMBOL=${GEOMETRY_DOCKER_COMPOSE_AVAILABLE_SYMBOL:-"[d]"}
 
 
 geometry_prompt_docker_compose_setup() {
@@ -14,7 +14,7 @@ geometry_prompt_docker_compose_setup() {
 
 geometry_prompt_docker_compose_check() {
   # Do nothing if we don't have a compose file
-  [ -f $PWD/docker-compose.yml ] || return 1
+  [ -f "$PWD/docker-compose.yml" ] || return 1
 }
 
 geometry_prompt_docker_compose_render() {
@@ -26,7 +26,7 @@ geometry_prompt_docker_compose_render() {
     exited="$(prompt_geometry_colorize $GEOMETRY_DOCKER_MACHINE_FAILED_COLOR "$exited")"
   fi
 
-  echo "${GEOMETRY_GIT_SEPARATOR} ${GEOMETRY_DOCKER_COMPOSE_AVAILABLE_SYMBOL}  $running/$exited/$services"
+  echo " ${GEOMETRY_GIT_SEPARATOR} ${GEOMETRY_DOCKER_COMPOSE_AVAILABLE_SYMBOL} $running/$exited/$services"
 }
 
 docker_compose_count_services() {
@@ -41,4 +41,4 @@ docker_compose_count_running() {
   docker-compose ps | tail -n+3 | grep -cvE 'Exit \d+\b'
 }
 
-geometry_plugin_register docker_compose
+# geometry_plugin_register docker_compose
