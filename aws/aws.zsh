27da1fff7852c2cc42c0bcc040d71dc102372c01
Lachlan Donald <lachlan@ljd.cc>
2019-01-31 12:21:25 +1100

    Better aws-vault integration into geometry

--- aws/aws.zsh
+++ aws/aws.zsh
@@ -1,14 +1,8 @@
 
-# if [[ -n "$AWS_VAULT" ]] ; then
-#   export RPROMPT="<aws:$AWS_VAULT>"
-# fi
-
 export AWS_VAULT_PROMPT=osascript
 export AWS_VAULT_BIOMETRICS=true
 export AWS_VAULT_KEYCHAIN_NAME=login
 export AWS_ASSUME_ROLE_TTL=1h
-
-alias aws-vault-dev=$HOME/go/bin/aws-vault
 
 ecr-login() {
   local profile="${1:-}"
--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,3 +1,20 @@
+# aws_vault
+# A geometry plugin for showing the aws-vault session that is active
+
+geometry_prompt_aws_vault_setup() {
+  :
+}
+
+geometry_prompt_aws_vault_check() {
+  [ -n "${AWS_VAULT:-}" ] || return 1
+}
+
+geometry_prompt_aws_vault_render() {
+  echo "${GEOMETRY_GIT_SEPARATOR} aws:${AWS_VAULT}"
+}
+
+geometry_plugin_register aws_vault
+
 # git_identity
 # A geometry plugin for showing a locks symbol if there is a signing key for a repository
 
