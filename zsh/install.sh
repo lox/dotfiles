011ebe244ebb460ca48403bb06906da865032a63
Lachlan Donald <lachlan@ljd.cc>
2023-04-08 15:59:39 +0930

    Remove references to /usr/local/bin

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-if ! grep -q '/usr/local/bin/zsh' /etc/shells ; then
-  sudo sh -c 'echo "$(which zsh)" >> /etc/shells'
-  sudo chsh -s $(which zsh) $(whoami)
+zsh_bin="$(brew --prefix)/bin/zsh"
+
+if ! grep -q "$zsh_bin" /etc/shells ; then
+  sudo sh -c "printf '%s\n' '$zsh_bin'" >> /etc/shells
+  sudo chsh -s "$zsh_bin" "$(whoami)"
 fi
 
 antibody bundle <"$DOTFILES/zsh/bundles.txt" >~/.zsh_plugins.sh
