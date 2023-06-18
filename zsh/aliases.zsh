2170d44042acaef22f66dc2696c759bcbdfaaf5c
Lachlan Donald <lachlan@ljd.cc>
2023-06-18 21:32:59 +1000

    Move hermit shellhooks into zshrc

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,19 +2,12 @@
 # reload zsh config
 alias reload!='. ~/.zshrc'
 
-
-# Hermit shell hooks
-# See https://cashapp.github.io/hermit/usage/shell/
-if command -v hermit >/dev/null 2>&1; then
-    eval "$(hermit shell-hooks --print --zsh)"
-fi
-
 # Access tailscale command
 if [[ -f /Applications/Tailscale.app/Contents/MacOS/Tailscale ]] ; then
   alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
 fi
 
 # VSCode Insiders
-if command -v code-insiders >/dev/null 2>&1 && ! command -v code >/dev/null 2>&1; then
-    alias code=code-insiders
-fi
+# if command -v code-insiders >/dev/null 2>&1 && ! command -v code >/dev/null 2>&1; then
+#     alias code=code-insiders
+# fi
--- zsh/hermit.zsh
+++
@@ -1,4 +0,0 @@
-if command -v hermit >/dev/null 2>&1; then
-    export HERMIT_ROOT_BIN="$(brew --prefix)/bin/hermit"
-    eval "$(hermit shell-hooks --print --zsh)" >/dev/null
-fi
