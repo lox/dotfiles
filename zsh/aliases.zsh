d35fdc83c8d9b1c27457d51fda94d0ae5a8a97dd
Lachlan Donald <lachlan@ljd.cc>
2023-06-19 08:15:56 +1000

    Remove insiders alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,8 +6,3 @@ alias reload!='. ~/.zshrc'
 if [[ -f /Applications/Tailscale.app/Contents/MacOS/Tailscale ]] ; then
   alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
 fi
-
-# VSCode Insiders
-# if command -v code-insiders >/dev/null 2>&1 && ! command -v code >/dev/null 2>&1; then
-#     alias code=code-insiders
-# fi
