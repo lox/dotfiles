5db8f1d6d7ef3023e7db0b28be170efb5787a4c1
Lachlan Donald <lachlan@ljd.cc>
2024-04-09 14:17:28 +1000

    Add iterm path to tab

---
+++ zsh/iterm.zsh
@@ -0,0 +1,9 @@
+set-window-title() {
+  # /Users/lachlan/Projects/dotfiles -> ~/p/dotfiles
+  window_title="\e]0;${${PWD/#"$HOME"/~}/Projects/P}\a"
+  echo -ne "$window_title"
+}
+
+PR_TITLEBAR=''
+set-window-title
+add-zsh-hook precmd set-window-title
