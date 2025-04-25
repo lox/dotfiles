9ea93498a8aedf7297891942a6b8f4f197553706
Lachlan Donald <lachlan@ljd.cc>
2025-04-25 16:21:20 -0700

    Add fancy k8s stuff

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,3 @@
-
 # reload zsh config
 alias reload!='. ~/.zshrc'
 
@@ -6,3 +5,59 @@ alias reload!='. ~/.zshrc'
 if [[ -f /Applications/Tailscale.app/Contents/MacOS/Tailscale ]] ; then
   alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
 fi
+
+# kubectl with kubecolor
+alias k=kubecolor
+alias kc=kubecolor
+alias kg='kubecolor get'
+alias kd='kubecolor describe'
+alias kx='kubecolor exec -it'
+alias kl='kubecolor logs -f'
+alias kp='kubecolor get pods'
+alias kn='kubecolor get namespaces'
+alias kctx='kubecolor config use-context'
+alias kns='kubecolor config set-context --current --namespace'
+
+# kubectl with fzf
+kf() {
+  local resource
+  resource=$(kubecolor api-resources --output=name | fzf --prompt="Select resource type: ")
+  [ -n "$resource" ] && kubecolor get "$resource" | fzf --header-lines=1
+}
+
+# kubectl port-forward with fzf
+kpf() {
+  local pod
+  pod=$(kubecolor get pods --output=name | fzf --prompt="Select pod: ")
+  [ -n "$pod" ] && kubecolor port-forward "$pod" "$@"
+}
+
+# kubectl exec with fzf
+kxf() {
+  local pod
+  pod=$(kubecolor get pods --output=name | fzf --prompt="Select pod: ")
+  [ -n "$pod" ] && kubecolor exec -it "$pod" -- "$@"
+}
+
+# kubectl logs with fzf
+klf() {
+  local pod
+  pod=$(kubecolor get pods --output=name | fzf --prompt="Select pod: ")
+  [ -n "$pod" ] && kubecolor logs -f "$pod" "$@"
+}
+
+# kubectl delete with fzf
+kdf() {
+  local resource
+  resource=$(kubecolor api-resources --output=name | fzf --prompt="Select resource type: ")
+  [ -n "$resource" ] && {
+    local selection
+    selection=$(kubecolor get "$resource" | fzf --header-lines=1 --multi)
+    [ -n "$selection" ] && {
+      local names
+      names=$(echo "$selection" | awk '{print $1}')
+      echo "Deleting: $names"
+      echo "$names" | xargs -I {} kubecolor delete "$resource" {}
+    }
+  }
+}
