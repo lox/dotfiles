daac198eec7eee4c844a0c7fac1a960d2918a1b7
Lachlan Donald <lachlan@ljd.cc>
2026-02-13 08:34:13 +1100

    chore: remove wtp, unused

--- wtp/completion.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-# wtp completion - loads after compinit
-# Use absolute path to avoid PATH resolution issues during shell reload
-if [[ -x "$HOME/bin/wtp" ]]; then
-  eval "$("$HOME/bin/wtp" completion zsh 2>/dev/null)"
-elif command -v wtp >/dev/null 2>&1; then
-  eval "$(wtp completion zsh 2>/dev/null)"
-fi
